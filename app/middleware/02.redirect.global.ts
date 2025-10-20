0e775bce9191bbad0e6aa2b4cfc529d81230057c
AlphaWeb <alphaweb@castelec.com>
2025-10-20 10:24:06 -0600

    Se quita middleware que estaba afectando renderizacion de sidebar

--- app/middleware/02.redirect.global.ts
+++
@@ -1,7 +0,0 @@
-export default defineNuxtRouteMiddleware((to, _) => {
-    const layoutStore = useLayoutStore();
-    const breakpoint = useBreakpoint();
-
-    // Cuando esta en modo movil, al dar click al menu lateral, se cierre automaticamente.
-    if (to.path !== '/' && breakpoint.down('sm')) layoutStore.closeSidebar();
-});
